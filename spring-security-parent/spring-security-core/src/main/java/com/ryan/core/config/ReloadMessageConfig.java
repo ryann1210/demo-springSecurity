c0f94ac8a8e60c628d54d73e39918891be97f9ed
ryan <ryann1210@163.com>
2020-05-11 20:25:46 +0800

    提示文字切换中文

---
+++ spring-security-parent/spring-security-core/src/main/java/com/ryan/core/config/ReloadMessageConfig.java
@@ -0,0 +1,20 @@
+package com.ryan.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class ReloadMessageConfig {
+
+    /**
+     * 加载中文认证信息
+     * @return
+     */
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:org/springframework/security/messages_zh_CN");
+        return messageSource;
+    }
+}
